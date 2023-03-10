cdeaa4afc1dc9d654c3675c2b8b9dd57d5180853
Travis <Toooo123@live.ca>
2023-03-09 18:33:24 -0800

    Add morsecode encoding codes

--- morse-code.c
+++ morse-code.c
@@ -9,6 +9,58 @@
 
 #define MY_DEVICE_FILE  "morse-code"
 
+// Morse Code Encodings (from http://en.wikipedia.org/wiki/Morse_code)
+//   Encoding created by Brian Fraser. Released under GPL.
+//
+// Encoding description:
+// - msb to be output first, followed by 2nd msb... (left to right)
+// - each bit gets one "dot" time.
+// - "dashes" are encoded here as being 3 times as long as "dots". Therefore
+//   a single dash will be the bits: 111.
+// - ignore trailing 0's (once last 1 output, rest of 0's ignored).
+// - Space between dashes and dots is one dot time, so is therefore encoded
+//   as a 0 bit between two 1 bits.
+//
+// Example:
+//   R = dot   dash   dot       -- Morse code
+//     =  1  0 111  0  1        -- 1=LED on, 0=LED off
+//     =  1011 101              -- Written together in groups of 4 bits.
+//     =  1011 1010 0000 0000   -- Pad with 0's on right to make 16 bits long.
+//     =  B    A    0    0      -- Convert to hex digits
+//     = 0xBA00                 -- Full hex value (see value in table below)
+//
+// Between characters, must have 3-dot times (total) of off (0's) (not encoded here)
+// Between words, must have 7-dot times (total) of off (0's) (not encoded here).
+//
+static unsigned short morsecode_codes[] = {
+		0xB800,	// A 1011 1
+		0xEA80,	// B 1110 1010 1
+		0xEBA0,	// C 1110 1011 101
+		0xEA00,	// D 1110 101
+		0x8000,	// E 1
+		0xAE80,	// F 1010 1110 1
+		0xEE80,	// G 1110 1110 1
+		0xAA00,	// H 1010 101
+		0xA000,	// I 101
+		0xBBB8,	// J 1011 1011 1011 1
+		0xEB80,	// K 1110 1011 1
+		0xBA80,	// L 1011 1010 1
+		0xEE00,	// M 1110 111
+		0xE800,	// N 1110 1
+		0xEEE0,	// O 1110 1110 111
+		0xBBA0,	// P 1011 1011 101
+		0xEEB8,	// Q 1110 1110 1011 1
+		0xBA00,	// R 1011 101
+		0xA800,	// S 1010 1
+		0xE000,	// T 111
+		0xAE00,	// U 1010 111
+		0xAB80,	// V 1010 1011 1
+		0xBB80,	// W 1011 1011 1
+		0xEAE0,	// X 1110 1010 111
+		0xEBB8,	// Y 1110 1011 1011 1
+		0xEEA0	// Z 1110 1110 101
+};
+
 /******************************************************
  * Callbacks
  ******************************************************/
